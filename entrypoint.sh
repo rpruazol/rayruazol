1fb1d82e7e18709497c5b542b7ef17bd12e1f497
rpruazol <rpruazol@gmail.com>
2019-02-26 22:54:23 -0800

    docker installation

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+cd /src
+
+# coverage run manage.py test -v 2 && \
+# coverage report
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:8000
